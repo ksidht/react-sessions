9df8040f4d4dc7e1baf0ab60fe1c0cd2689c5e3c
Siddharth Kamble <siddharth.kamble@maximess.com>
2019-07-26 17:48:42 +0530

    Day 2: JSX and functional component

---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+function Hello(props) {
+  return (<div>
+  <h1>Hello! {props.name}</h1>
+  <h2>Good to see you here.</h2>
+  </div>);
+}
+
+export default Hello;
